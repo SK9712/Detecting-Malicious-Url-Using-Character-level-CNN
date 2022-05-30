39b9cd4973f1ad25ad1b3844aaf2398e6547f4c1
Sushant Kumar <susk1297@gmail.com>
2022-05-30 15:47:37 +0530

    Update dataset_creator.py

--- dataset_creator.py
+++ dataset_creator.py
@@ -1,6 +1,6 @@
 f = open("Data/URL.txt", "a")
-#sas=input("Enter the url\n")
-#sas+="\n"
+#inp=input("Enter the url\n")
+#inp+="\n"
 import sys
 	
 print("Enter the data")
@@ -8,9 +8,9 @@ data = sys.stdin.read()   # Use Ctrl d to stop the input
 data=list(data)
 indices = [i for i, x in enumerate(data) if x == "\n"]
 j=0
-sas=input("enter label\n")
+inp=input("enter label\n")
 for i in indices:
 	ted=i+j
-	data.insert(ted,"\t"+sas)
+	data.insert(ted,"\t"+inp)
 	j+=1
 f.write("".join(data))
